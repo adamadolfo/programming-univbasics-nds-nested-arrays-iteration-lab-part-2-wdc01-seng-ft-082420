fc7ad81dcaa7cb4d282a671569222708095a5670
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-10 15:59:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ outer_results = []
 row_index = 0 
 while row_index < src.count do
   element_index = 0 
-  lowest_number_element = ""
+  lowest_number_element = 
   while element_index < src[row_index].count do
     if src[row_index][element_index] < lowest_number_element
       lowest_number_element = src[row_index][element_index]
